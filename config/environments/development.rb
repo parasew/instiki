1bb4747a0fd40c27666d7299460e47c4af2495e8
Alexey Verkhovsky <averkhov@thoughtworks.com>
2005-03-25 22:55:59 +0000

    Fixed development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,4 +2,4 @@ Dependencies.mechanism = :require
 ActionController::Base.consider_all_requests_local = true
 ActionController::Base.perform_caching = false
 BREAKPOINT_SERVER_PORT = 42531
-ActionController::Base.logger.level = Logger::DEBUG
+INSTIKI_DEBUG_LOG = true unless defined? INSTIKI_DEBUG_LOG
