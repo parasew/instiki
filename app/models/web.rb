6367ef38c432b7727e487a4bb1eb41ff28b5028d
Alexey Verkhovsky <averkhov@thoughtworks.com>
2005-03-29 06:00:07 +0000

    Small refactoring in web.rb (replaced three lines with one)

--- app/models/web.rb
+++ app/models/web.rb
@@ -56,8 +56,7 @@ class Web
     wiki.file_yard(self).has_file?(name)
   end
 
-  def make_file_link(mode, name, text, base_url)
-    link = CGI.escape(name)
+  def make_file_link(mode, link, text, base_url)
     case mode
     when :export
       if has_file?(name) then "<a class=\"existingWikiWord\" href=\"#{link}.html\">#{text}</a>"
@@ -81,23 +80,23 @@ class Web
   # It should not be used in menus, templates and such - instead, use link_to_page helper
   def make_link(name, text = nil, options = {})
     text = CGI.escapeHTML(text || WikiWords.separate(name))
+    link = CGI.escape(name)
     mode = options[:mode] || :show
     base_url = options[:base_url] || '..'
     link_type = options[:link_type] || :show
     case link_type.to_sym
     when :show
-      make_page_link(mode, name, text, base_url)
+      make_page_link(mode, link, text, base_url)
     when :file
-      make_file_link(mode, name, text, base_url)
+      make_file_link(mode, link, text, base_url)
     when :pic
-      make_pic_link(mode, name, text, base_url)
+      make_pic_link(mode, link, text, base_url)
     else
       raise "Unknown link type: #{link_type}"
     end
   end
 
-  def make_page_link(mode, name, text, base_url)
-    link = CGI.escape(name)
+  def make_page_link(mode, link, text, base_url)
     case mode.to_sym
     when :export
       if has_page?(name) then %{<a class="existingWikiWord" href="#{link}.html">#{text}</a>}
@@ -114,8 +113,7 @@ class Web
     end
   end
 
-  def make_pic_link(mode, name, text, base_url)
-    link = CGI.escape(name)
+  def make_pic_link(mode, link, text, base_url)
     case mode.to_sym
     when :export
       if has_file?(name) then %{<img alt="#{text}" src="#{link}" />}
