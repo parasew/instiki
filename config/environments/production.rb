634bc67342c325c828c35c155bee32b487fe60e1
Rick Okin <rickokin@yahoo.com>
2005-08-09 02:28:37 +0000

    More lenient parsing of blocked_ips.txt and spam_patterns.txt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,15 @@ ActionController::Base.perform_caching = false
 
 spam_patterns_filename = RAILS_ROOT + '/config/spam_patterns.txt'
 if File.exists? spam_patterns_filename
-  SPAM_PATTERNS = File.readlines(spam_patterns_filename).select { |line| line != '' }.map { 
-      |line| Regexp.new(line) }
+  SPAM_PATTERNS = File.readlines(spam_patterns_filename).delete_if { |line| line.strip.empty? }.map { 
+      |line| Regexp.new(line.strip) }
 end
 
 blocked_ips_filename = RAILS_ROOT + '/config/blocked_ips.txt'
 if File.exists? blocked_ips_filename
-  BLOCKED_IPS = File.readlines(blocked_ips_filename).select { |line| line != '' }
+  BLOCKED_IPS = File.readlines(blocked_ips_filename).delete_if { |line| line.strip.empty? }.map { 
+      |line| line.strip }
 end
+
+require 'breakpoint'
+breakpoint
