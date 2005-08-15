7493bc8af9ad8f93decb7fb1b529dd00104da748
Alexey Verkhovsky <averkhov@thoughtworks.com>
2005-08-15 00:24:15 +0000

    Fixed exports and other file downloads

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -67,6 +67,7 @@ class ApplicationController < ActionController::Base
 
   def send_file(file, options = {})
     options[:type] ||= (FILE_TYPES[File.extname(file)] || 'application/octet-stream')
+    options[:stream] = false
     super(file, options)
   end
 
