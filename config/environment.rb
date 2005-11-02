0b05368359260ed5a97643ba2fed28930edbc68e
Alexey Verkhovsky <averkhov@thoughtworks.com>
2005-11-02 05:34:07 +0000

    [BREAKS BUILD] Database schema, Rakefile and contents of config directory are brought inline with 0.14.2 defaults

--- config/environment.rb
+++ config/environment.rb
@@ -1,95 +1,34 @@
-# Load the Rails framework and configure your application.
-# You can include your own configuration at the end of this file.
-#
-# Be sure to restart your webserver when you modify this file.
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
 
-if RUBY_VERSION < '1.8.2' 
-  puts 'Instiki requires Ruby 1.8.2+'
-  exit
+Rails::Initializer.run do |config|
+  # Skip frameworks you're not going to use
+  config.frameworks -= [ :action_web_service, :action_mailer ]
+
+  # Use the database for sessions instead of the file system
+  # (create the session table with 'rake create_sessions_table')
+  config.action_controller.session_store = :active_record_store
+
+  # Enable page/fragment caching by setting a file-based store
+  # (remember to create the caching directory and make it readable to the application)
+  # config.action_controller.fragment_cache_store = :file_store, "#{RAILS_ROOT}/cache"
+
+  # Activate observers that should always be running
+  # config.active_record.observers = :cacher, :garbage_collector
+
+  # Make Active Record use UTC-base instead of local time
+  config.active_record.default_timezone = :utc
+  
+  # Use Active Record's schema dumper instead of SQL when creating the test database
+  # (enables use of different database adapters for development and test environments)
+  config.active_record.schema_format = :ruby
+
+  # See Rails::Configuration for more options
 end
+
+# Instiki-specific configuration below
+require_dependency 'instiki_errors'
 
 # Enable UTF-8 support
 $KCODE = 'u'
 require 'jcode'
-
-# The path to the root directory of your application.
-RAILS_ROOT = File.expand_path(File.dirname(__FILE__) + '/../')
-
-# The environment your application is currently running.  Don't set
-# this here; put it in your webserver's configuration as the RAILS_ENV
-# environment variable instead.
-#
-# See config/environments/*.rb for environment-specific configuration.
-RAILS_ENV  = ENV['RAILS_ENV'] || 'development'
-
-# Load the Rails framework.  Mock classes for testing come first.
-ADDITIONAL_LOAD_PATHS = ["#{RAILS_ROOT}/test/mocks/#{RAILS_ENV}"]
-
-# Then model subdirectories.
-ADDITIONAL_LOAD_PATHS.concat(Dir["#{RAILS_ROOT}/app/models/[_a-z]*"])
-ADDITIONAL_LOAD_PATHS.concat(Dir["#{RAILS_ROOT}/components/[_a-z]*"])
-
-# Followed by the standard includes.
-ADDITIONAL_LOAD_PATHS.concat %w(
-  app 
-  app/models 
-  app/controllers 
-  app/helpers 
-  app/apis 
-  components 
-  config 
-  lib 
-  vendor 
-  vendor/rails/railties
-  vendor/rails/railties/lib
-  vendor/rails/actionpack/lib
-  vendor/rails/activesupport/lib
-  vendor/rails/activerecord/lib
-  vendor/rails/actionmailer/lib
-  vendor/rails/actionwebservice/lib
-  vendor/RedCloth-3.0.3/lib
-  vendor/rubyzip-0.5.8/lib
-).map { |dir| "#{RAILS_ROOT}/#{dir}" }.select { |dir| File.directory?(dir) }
-
-# Prepend to $LOAD_PATH
-ADDITIONAL_LOAD_PATHS.reverse.each { |dir| $:.unshift(dir) if File.directory?(dir) }
-
-# Require Rails libraries.
-require 'rubygems' unless File.directory?("#{RAILS_ROOT}/vendor/rails")
-
-require 'active_support'
-require 'active_record'
-require 'action_controller'
-require 'action_mailer'
-require 'action_web_service'
-
-# Environment-specific configuration.
-require_dependency "environments/#{RAILS_ENV}"
-ActiveRecord::Base.configurations = File.open("#{RAILS_ROOT}/config/database.yml") { |f| YAML::load(f) }
-ActiveRecord::Base.establish_connection
-
-# Configure defaults if the included environment did not.
-begin
-  RAILS_DEFAULT_LOGGER = Logger.new("#{RAILS_ROOT}/log/#{RAILS_ENV}.log")
-  RAILS_DEFAULT_LOGGER.level = (RAILS_ENV == 'production' ? Logger::INFO : Logger::DEBUG)
-rescue StandardError
-  RAILS_DEFAULT_LOGGER = Logger.new(STDERR)
-  RAILS_DEFAULT_LOGGER.level = Logger::WARN
-  RAILS_DEFAULT_LOGGER.warn(
-    "Rails Error: Unable to access log file. Please ensure that log/#{RAILS_ENV}.log exists and is chmod 0666. " +
-    "The log level has been raised to WARN and the output directed to STDERR until the problem is fixed."
-  )
-end
-
-[ActiveRecord, ActionController, ActionMailer].each { |mod| mod::Base.logger ||= RAILS_DEFAULT_LOGGER }
-[ActionController, ActionMailer].each { |mod| mod::Base.template_root ||= "#{RAILS_ROOT}/app/views/" }
-
-# Set up routes.
-ActionController::Routing::Routes.reload
-
-Controllers = Dependencies::LoadingModule.root(
-  File.join(RAILS_ROOT, 'app', 'controllers'),
-  File.join(RAILS_ROOT, 'components')
-)
-
-require_dependency 'instiki_errors'
