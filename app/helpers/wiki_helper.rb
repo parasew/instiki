e636d6fa32e3471581225f9f41410e6314058497
Jacques Distler <distler@golem.ph.utexas.edu>
2007-02-15 08:26:47 -0600

    Valid XHTML? Couldn't hurt!

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -24,23 +24,23 @@ module WikiHelper
   def edit_page
     link_text = (@page.name == "HomePage" ? 'Edit Page' : 'Edit')
     link_to(link_text, {:web => @web.address, :action => 'edit', :id => @page.name}, 
-        {:class => 'navlink', :accesskey => 'E', :name => 'edit'})
+        {:class => 'navlink', :accesskey => 'E', :id => 'edit'})
   end
 
   def edit_web
     link_to('Edit Web', {:web => @web.address, :action => 'edit_web'}, 
-        {:class => 'navlink', :accesskey => 'W', :name => 'edit_web'})
+        {:class => 'navlink', :accesskey => 'W', :id => 'edit_web'})
   end
             
   def forward
     if @revision_number < @page.revisions.length - 1
       link_to('Forward in time', 
           {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number + 1},
-          {:class => 'navlink', :accesskey => 'F', :name => 'to_next_revision'}) + 
+          {:class => 'navlink', :accesskey => 'F', :id => 'to_next_revision'}) + 
           " <small>(#{@revision.page.revisions.length - @revision_number} more)</small> "
     else
         link_to('Forward in time', {:web => @web.address, :action => 'show', :id => @page.name},
-            {:class => 'navlink', :accesskey => 'F', :name => 'to_next_revision'}) +
+            {:class => 'navlink', :accesskey => 'F', :id => 'to_next_revision'}) +
             " <small> (to current)</small>"
     end
   end
@@ -48,7 +48,7 @@ module WikiHelper
   def back_for_revision
     link_to('Back in time',
         {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number - 1},
-        {:class => 'navlink', :name => 'to_previous_revision'}) + 
+        {:class => 'navlink', :id => 'to_previous_revision'}) + 
         " <small>(#{@revision_number - 1} more)</small>"
   end
 
@@ -56,32 +56,32 @@ module WikiHelper
     link_to('Back in time', 
         {:web => @web.address, :action => 'revision', :id => @page.name, 
         :rev => @page.revisions.length - 1},
-        {:class => 'navlink', :accesskey => 'B', :name => 'to_previous_revision'}) +
+        {:class => 'navlink', :accesskey => 'B', :id => 'to_previous_revision'}) +
         " <small>(#{@page.revisions.length - 1} #{@page.revisions.length - 1 == 1 ? 'revision' : 'revisions'})</small>"
   end
   
   def current_revision
     link_to('See current', {:web => @web.address, :action => 'show', :id => @page.name},
-        {:class => 'navlink', :name => 'to_current_revision'})
+        {:class => 'navlink', :id => 'to_current_revision'})
   end
   
   def see_or_hide_changes_for_revision
     link_to(@show_diff ? 'Hide changes' : 'See changes', 
         {:web => @web.address, :action => 'revision', :id => @page.name, :rev => @revision_number, 
          :mode => (@show_diff ? nil : 'diff') },
-        {:class => 'navlink', :accesskey => 'C', :name => 'see_changes'})
+        {:class => 'navlink', :accesskey => 'C', :id => 'see_changes'})
   end
 
   def see_or_hide_changes_for_page
     link_to(@show_diff ? 'Hide changes' : 'See changes', 
         {:web => @web.address, :action => 'show', :id => @page.name, :mode => (@show_diff ? nil : 'diff') },
-        {:class => 'navlink', :accesskey => 'C', :name => 'see_changes'})
+        {:class => 'navlink', :accesskey => 'C', :id => 'see_changes'})
   end
   
   def rollback
     link_to('Rollback', 
         {:web => @web.address, :action => 'rollback', :id => @page.name, :rev => @revision_number},
-        {:class => 'navlink', :name => 'rollback'})
+        {:class => 'navlink', :id => 'rollback'})
   end
 
   
