8fb8517156eb7bf220c8284f8a898f5fbcfb4b6b
Michal Wlodkowski <mw@mutti.jp>
2007-02-27 21:56:13 +0000

    css updates

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function toggleView(id)
+{
+	(document.getElementById(id).style.display == 'block') ? document.getElementById(id).style.display='none' : document.getElementById(id).style.display='block';
+}
