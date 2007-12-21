e74deb0cfbf1e147db81f039b9ab8d79c7123c37
Jacques Distler <distler@golem.ph.utexas.edu>
2007-12-21 08:53:45 -0600

    Unit test
    Add a unit test for previous WikiWord fix.

--- test/unit/page_renderer_test.rb
+++ test/unit/page_renderer_test.rb
@@ -76,6 +76,11 @@ class PageRendererTest < Test::Unit::TestCase
         "#My Headline#\n\nthat SmartEngineGUI")
   
     assert_markup_parsed_as(
+        %{<p>SVG <animateColor title="MathML"><span class="newWikiWord">} +
+        %{Math ML<a href="../show/MathML">?</a></span></animateColor></p>}, 
+        "SVG <animateColor title='MathML'>MathML</animateColor>")
+  
+    assert_markup_parsed_as(
         %{<div class="maruku-equation"><math class="maruku-mathml" display="block" } +
         %{xmlns="http://www.w3.org/1998/Math/MathML"><mi>sin</mi><mo stretchy="false">} +
         %{(</mo><mi>x</mi><mo stretchy="false">)</mo><semantics><annotation-xml encoding="SVG1.1">} +
