4936bea13fee6220a2ecdef0ae93d101b20d6d3e
Jacques Distler <distler@golem.ph.utexas.edu>
2009-01-11 13:49:58 -0600

    Boneheaded
    Remove some nonexistent callbacks, added in Revisions 265, 288.

--- app/controllers/web_sweeper.rb
+++ app/controllers/web_sweeper.rb
@@ -19,14 +19,6 @@ class WebSweeper < ActionController::Caching::Sweeper
     end
   end
 
-  def after_remove_orphaned_pages(web)
-    expire_cached_summary_pages(web)
-  end
-
-  def after_remove_orphaned_pages_in_category(web)
-    expire_cached_summary_pages(web)
-  end
-  
   def after_destroy(record)
     if record.is_a?(Web)
       expire_cached_summary_pages(record)
