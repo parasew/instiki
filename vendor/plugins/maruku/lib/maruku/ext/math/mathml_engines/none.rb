ce8578d2d0fb4e529c6ee456acd37cd27f4ad560
Jacques Distler <distler@golem.ph.utexas.edu>
2010-06-19 03:02:15 -0500

    Some Maruku Regexp Refactoring

--- vendor/plugins/maruku/lib/maruku/ext/math/mathml_engines/none.rb
+++ vendor/plugins/maruku/lib/maruku/ext/math/mathml_engines/none.rb
@@ -1,14 +1,14 @@
 module MaRuKu; module Out; module HTML
 
+  require 'maruku/string_utils'
+
 	def convert_to_mathml_none(kind, tex)
 		# You can: either return a REXML::Element
 		#    return Element.new 'div'    
 		# or return an empty array on error
 		#    return []  
 		# or have a string parsed by REXML:
-		mathml = "<code>#{tex.gsub( /&/, "&amp;" ).
-		  gsub( /</, "&lt;" ).gsub( />/, "&gt;"  ).
-		  gsub(/'/, "&#39;" ).gsub(/"/, "&quot;" )}</code>"
+		mathml = "<code>#{html_escape(tex)}</code>"
 		return Document.new(mathml).root
 	end
 
